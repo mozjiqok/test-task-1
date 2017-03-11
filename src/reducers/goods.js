b1d45823af1040dec52045d30e63cd713f033b78
mozjiqok <moskxxi@gmail.com>
2017-03-11 17:12:16 +1000

    Added initial state with some goods

--- src/reducers/goods.js
+++ src/reducers/goods.js
@@ -1,3 +1,10 @@
-export default (state = [], action = {}) => {
+const initialState=[
+	{id:1,name:"Товар 1",cost:2000,price:2500},
+	{id:2,name:"Товар 2",cost:2200,price:2700},
+	{id:3,name:"Товар 3",cost:2200,price:2700},
+	{id:4,name:"Товар 4",cost:2200,price:2700}
+];
+
+export default (state = initialState, action = {}) => {
   return state;
 }
