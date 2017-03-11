de8705e52573efa497a8443eb013683ea6e960ef
mozjiqok <moskxxi@gmail.com>
2017-03-11 16:05:03 +1000

    Main component structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
+	
   render() {
     return (
-      <div>
-          <h2>Welcome to my app</h2>
+      <div className="container">
+				<div className="">
+					<div className="">
+						My-app
+					</div>
+					<div className="">
+						<button>Добавить товар</button>
+						<button>Добавить категорию</button>
+					</div>
+				</div>
+				<div className="">
+					<div className="">
+						Список категорий
+					</div>
+					<div className="">
+						Таблица товаров
+					</div>
+				</div>
       </div>
     );
   }
+	
 }
 
 export default App;
