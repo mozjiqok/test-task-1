7da184f91ff3a4d0baf74d2203b59efe58b74357
mozjiqok <moskxxi@gmail.com>
2017-03-13 19:16:32 +1000

    Setup backend

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/*', (req, res) => {
+	res.send('hello world');
+});
+
+app.listen(9000, () => console.log('Running on localhost:9000'));
