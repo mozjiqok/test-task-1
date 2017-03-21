2237b6303930a5b6107a764f0ed5b5ac9be31205
mozjiqok <moskxxi@gmail.com>
2017-03-21 17:09:23 +1000

    Added user reducer to root reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import goods from './reducers/goods';
 import categs from './reducers/categs';
 import fetchingState from './reducers/fetchingState';
+import user from './reducers/user';
 
 const rootReducer = combineReducers({
   goods,
 	categs,
-	fetchingState
+	fetchingState,
+	user
 });
 
 export default rootReducer;
